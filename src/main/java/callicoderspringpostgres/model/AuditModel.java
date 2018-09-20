74e7dd0a4ccaefefe150bf0dae5e10de2fa4cdb1
pjbielec <przemyslaw.bielec@core3.pl>
2018-09-20 16:02:08 +0200

    In AuditModel added createdAt

--- src/main/java/callicoderspringpostgres/model/AuditModel.java
+++ src/main/java/callicoderspringpostgres/model/AuditModel.java
@@ -3,6 +3,7 @@ package callicoderspringpostgres.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -20,6 +21,11 @@ import java.util.Date;
 @Getter
 @Setter
 public abstract class AuditModel implements Serializable {
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "createdAt", nullable = false, updatable = false)
+    @CreatedDate
+    private Date createdAt;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "updatedAt", nullable = false)
